7f9764985510a61838f2cfad3acc27e2f43ec7ba
Michael Washer <michael.washer@icloud.com>
2020-06-08 18:10:36 +1200

    Add Context to Application Layer
    Add Context to the Application Layer
    Add Drawable Object as Brick
    Separate Brick and Platform Abstraction

--- app/src/main/java/com/example/ballgame/Resources/Brick.java
+++ app/src/main/java/com/example/ballgame/Resources/Brick.java
@@ -7,178 +7,22 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 
+import com.example.ballgame.MyApplication;
 import com.example.ballgame.interfaces.Collider;
 import com.example.ballgame.R;
 
-public class Brick extends DrawableObject implements Collider {
-    private V2 a, b, c, d;          // The corners
-    private V2 pab, pbd, pdc, pca;  // Perpendiculars to the sides
-    private float boarderWidth = 5;
-    Context context;
-    // TODO Rewrite this whole class to use real names Only 4 valeus are need, top left bottom right
-    // Not drawing misc shapes
-    ///////////////////// PRIVATE HELPER METHODS ////////////////////////
-    //TODO this is reusable Debug code. Remvoe when complete
-//    Drawable d = getResources().getDrawable(R.drawable.foobar, null);
-//d.setBounds(left, top, right, bottom);
-//d.draw(canvas);
-
-    // The Brick is R.drawable.brick
-
-    // Calculate reflection on one edge
-    private boolean reflectLine(V2 centre, V2 direction, float radius, V2 p, V2 q, V2 ppq) {
-        if (!inside(centre, radius, p, q, ppq)) {
-            float divisor, gamma, lambda;
-            V2 pp, qq;
-            pp = V2.add(p, V2.multiply(ppq, radius));
-            qq = V2.add(q, V2.multiply(ppq, radius));
-            divisor = direction.y * (pp.x - qq.x) - direction.x * (pp.y - qq.y);
-            if (divisor != 0) {
-                gamma = (direction.y * (pp.x - centre.x) - direction.x * (pp.y - centre.y)) / divisor;
-                if (gamma >= 0 && gamma <= 1) {
-                    lambda = ((pp.y - centre.y) * (pp.x - qq.x) - (pp.x- centre.x) * (pp.y - qq.y)) / divisor;
-                    if (lambda > 0 && lambda <= 1) {
-                        //centre.x += lambda * direction.x;
-                        //centre.y += lambda * direction.y;
-                        // Update direction in place
-                        V2 newdir = V2.subtract(direction, V2.multiply(V2.multiply(ppq, V2.dot(direction, ppq)), 2));
-                        direction.x = newdir.x;
-                        direction.y = newdir.y;
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }   
-
-    // Calculate reflection on a curved corner
-    private boolean reflectCurve(V2 centre, V2 direction, float radius, V2 p, V2 q, V2 r, V2 ppq, V2 prp) {
-        if (V2.subtract(centre, p).length() >= radius) {
-            // The ball is outside the circle(radius) about p
-            float deltax, deltay, lensq, bb, cc, det, lambda;
-            deltax = p.x - centre.x;
-            deltay = p.y - centre.y;
-            lensq = (direction.x * direction.x + direction.y * direction.y);
-            bb = (direction.x * deltax + direction.y * deltay) / lensq;
-            cc = (deltax * deltax + deltay * deltay - radius * radius) / lensq;
-            det = bb * bb - cc;
-            if (det >= 0) {
-                lambda = bb - (float) Math.sqrt(det);
-                if (lambda >= 0 && lambda <= 1) {
-                    V2 hit = V2.add(centre, V2.multiply(direction, lambda));
-                    if (!inside(hit, 0, p, q, ppq) && !inside(hit, 0, r, p, prp)) {
-                        V2 normal = V2.subtract(hit, p).normalize();
-                        V2 newdir = V2.subtract(direction, V2.multiply(V2.multiply(normal, V2.dot(direction, normal)), 2));
-                        direction.x = newdir.x;
-                        direction.y = newdir.y;
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    // Is a ball of radius r 'inside' with respect to one edge
-    private boolean inside(V2 x, float r, V2 p, V2 q, V2 ppq) {
-        return V2.dot(ppq, V2.subtract(x, V2.add(p, V2.multiply(ppq, r)))) < 0;
-    }
-
-    ////////////////// PUBLIC METHODS ////////////////////////////////////
+public class Brick extends DrawableRectangle {
 
     // Constructor
     public Brick(Context context, V2 corner, float width, float height) {
-        a = corner;
-        b = V2.add(a, new V2(width, 0));
-        c = V2.add(a, new V2(0, height));
-        d = V2.add(a, new V2(width, height));
-        pab = V2.subtract(a, c).normalize();
-        pbd = V2.subtract(b, a).normalize();
-        pdc = pab.negate();
-        pca = pbd.negate();
-        this.context = context;
+        super(context, corner, width, height);
     }
     public Brick(V2 corner, float width, float height) {
-        this(null, corner, width, height);
+        this(MyApplication.getAppContext(), corner, width, height);
     }
-
-    // Reflection of a ball against the rectangle - updates 'direction'
-    @Override public void reflectBall(V2 centre, V2 direction, float radius) {
-        if (reflectLine(centre, direction, radius, a, b, pab)) return;
-        if (reflectLine(centre, direction, radius, b, d, pbd)) return;
-        if (reflectLine(centre, direction, radius, d, c, pdc)) return;
-        if (reflectLine(centre, direction, radius, c, a, pca)) return;
-        if (reflectCurve(centre, direction, radius, b, d, a, pbd, pab)) return;
-        if (reflectCurve(centre, direction, radius, d, c, b, pdc, pbd)) return;
-        if (reflectCurve(centre, direction, radius, c, a, d, pdc, pca)) return;
-        if (reflectCurve(centre, direction, radius, a, b, c, pab, pca)) return;
-    }
-
-    // Does a ball intersect with the rectangle
-    @Override public boolean intersectBall(V2 centre, float radius) {
-        if (inside(centre, radius, a, b, pab)
-            && inside(centre, radius, b, d, pbd)
-            && inside(centre, radius, d, c, pdc)
-            && inside(centre, radius, c, a, pca)) {
-            if (!inside(centre, 0, a, b, pab)) {
-                if (!inside(centre, 0, c, a, pca)) {
-                    if (V2.subtract(centre, a).length() >= radius)
-                        return false;
-                } else if (!inside(centre, 0, b, d, pbd)) {
-                    if (V2.subtract(centre, b).length() >= radius)
-                        return false;
-                }
-            } else if (!inside(centre, 0, d, c, pdc)) {
-                if (!inside(centre, 0, c, a, pca)) {
-                    if (V2.subtract(centre, c).length() >= radius)
-                        return false;
-                } else if (!inside(centre, 0, b, d, pbd)) {
-                    if (V2.subtract(centre, d).length() >= radius)
-                        return false;
-                }
-            }
-            return true;
-        }
-        return false;
-    }
-    //Getters and Setters
-    public float getWidth()
-    {
-        return b.x - a.x;
-    }
-    public float getHeight()
-    {
-        return a.y - c.y;
-    }
-    public V2 getStartPointClone()
-    {
-        return new V2(a.x,a.y);
-    }
-    public V2 getStartPoint()
-    {
-        return a;
-    }
-
-    @Override
-    public void repositionRelative(int x, int y) {
-        //Point A
-        a.x += x;
-        a.y += y;
-        //Point B
-        b.x += x;
-        b.y += y;
-        //Point C
-        c.x += x;
-        c.y += y;
-        //Point D
-        d.x += x;
-        d.y += y;
-    }
-
     private void drawSprite(Canvas c){
-        Drawable brickSprite = context.getDrawable(R.drawable.brick);
-        brickSprite.setBounds((int)a.x, (int)a.y, (int)d.x, (int)d.y);
+        Drawable brickSprite = context.getDrawable(R.drawable.ic_brown_brick);
+        brickSprite.setBounds((int)this.a.x, (int)this.a.y, (int)this.d.x, (int)this.d.y);
         brickSprite.draw(c);
     }
     private void drawPaint(Canvas c, Paint p){
--- app/src/main/java/com/example/ballgame/Resources/Platform.java
+++ app/src/main/java/com/example/ballgame/Resources/Platform.java
@@ -1,15 +1,38 @@
 package com.example.ballgame.Resources;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+
+import com.example.ballgame.MyApplication;
+import com.example.ballgame.R;
+import com.example.ballgame.interfaces.Collider;
+
 /**
  * Created by michaelwasher on 17/10/15.
  */
-public class Platform extends Brick{
+public class Platform extends DrawableRectangle {
 
-    public Platform(V2 corner, float width, float height)
-    {
-        super(corner, width, height);
+    public Platform(Context context, V2 corner, float width, float height) {
+        super(context, corner, width, height);
+    }
+    public Platform(V2 corner, float width, float height) {
+        this(MyApplication.getAppContext(), corner, width, height);
     }
 
-    //MOVING THE PLATFORM USING SENSORS
+    private void drawPaint(Canvas c, Paint p){
+        int originalColor = p.getColor();
+        c.drawRect(a.x, a.y, d.x, d.y, p);
+        p.setColor(Color.WHITE);
+        c.drawRect(a.x + boarderWidth, a.y + boarderWidth, d.x - boarderWidth, d.y - boarderWidth, p );
+        p.setColor(originalColor);
+    }
+    // Draw the rectangle
+    @Override
+    public void draw(Canvas c, Paint p) {
+        drawPaint(c, p);
+    }
 
 }
